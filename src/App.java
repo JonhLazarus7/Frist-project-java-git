0a92efb997f4b7c1ca3bab71c593a783bfadadac
João Lázaro <ijoaolazaro7@gmail.com>
2025-02-08 11:31:16 -0300

    Solicitação das notas como entrada

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+
+
+        System.out.print("Type your name: ");
+        String name = sc.nextLine();
+        System.out.print("Type your note: ");
+        Float note1 = sc.nextFloat();
+        System.out.print("Type your second note: ");
+        Float note2 = sc.nextFloat();
+        sc.close();
+        double average = (note1 + note2) / 2;
+        System.out.print("Olá," + name + "!");
+        System.out.print("Sua média é : " +average);
     }
 }
